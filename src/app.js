bef71d7f693782a29bbbdef9f542a6acee596d9a
Prakashdeshmuk <suhasdeshmukh100101@gmail.com>
2024-11-18 19:59:39 +0530

    utils added

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express, { urlencoded } from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({
+    limit:"16kb"
+}))
+
+// express.json is middleware and parse data in json format
+// app.use is only used when we using some configuration or middlewares
+
+app.use(urlencoded(
+    {
+        limit:"16kb",
+        extended:true
+    }
+))
+
+// urlencoded is middle that parse url data 
+
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import express from "express"
+
+const app = express()
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>
+{
+
+    app.on("error",(error)=>
+    {
+        console.log("Error in Express App Listening",error)
+        throw error
+    })
+    
+    app.listen(process.env.PORT || 8000,(req,res)=>
+    {
+        console.log(`App is Listening on Port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB Connection Error :",error)
+})
 
 // const app = express();
 
