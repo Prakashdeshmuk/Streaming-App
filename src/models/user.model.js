a011de11d1355156786f14c4ce191e2860d856eb
Prakashdeshmuk <suhasdeshmukh100101@gmail.com>
2024-11-19 16:47:29 +0530

    Added User Model and Video Model

---
+++ src/models/user.model.js
@@ -0,0 +1,112 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+const userSchema = new Schema(
+    {
+
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            index : true,
+            trim:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true
+        },
+        fullName:{
+            type:String,
+            required:true,
+            index : true,
+            trim:true
+        },
+        avatar:{
+            type:String,
+            required:true,
+        },
+        coverImage:
+        {
+            type:String,
+            required:true
+        },
+        watchHistory:
+        [
+            {
+                type : Schema.Types.ObjectId,
+                ref :"Video"
+            }
+        ],
+        password:{
+            type:String,
+            required:[true,"Password is Required"]
+        },
+        refreshToken:{
+            type:String,
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+
+// we saved encrypted password on database , before we saved data of database we encrypted the password
+// if we do this every time we saved document password gets encrypted so we want to encryted the password only if its first time saving password or 
+// we making changes in the password(forget password)
+
+userSchema.pre("save",async function(next)
+{   
+    if(!this.isModified("password")) next();
+
+    this.password = await bcrypt.hash(password,10);
+    next();
+})
+
+
+// we add method check is password is correct or not 
+
+userSchema.methods.isPasswordcorrect = async function (password) {
+    return await bcrypt.compare(password,this.password);
+}
+
+// this enough fast we do not added async before the fuction
+userSchema.methods.generateAccessToken = function()
+{
+    return jwt.sign(
+        {
+            _id : this._id,
+            email:this.email,
+            username:this.username,
+            fullName:this.fullName
+
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function()
+{
+    return jwt.sign(
+        {
+            _id : this._id,
+
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+userSchema.plugin(mongooseAggregatePaginate);
+export default User = mongoose.model("User",userSchema);
